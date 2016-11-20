9c7a0029de274809b44eb52710a220bf200ac5f1
snowdream <yanghui1986527@gmail.com>
2016-11-20 16:11:04 +0800

    Fix:  JCenterLibrary

--- app/src/main/java/com/github/snowdream/app/MainActivity.java
+++ app/src/main/java/com/github/snowdream/app/MainActivity.java
@@ -5,11 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.github.snowdream.lib.MyLibrary;
-import com.github.snowdream.lib_maven.LibraryMaven;
+import android.view.View;
+
+import com.github.snowdream.lib_jcenter.JCenterLibrary;
 
 public class MainActivity extends AppCompatActivity {
 
